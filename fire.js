9cc40bda4578c81f14e863ddf494a993598940b5
Gabriela Carvalho Camilo <gabriela.camilo@pca.com.br>
2020-02-20 14:16:12 -0300

    render fire structure

---
+++ fire.js
@@ -0,0 +1,38 @@
+const firePixelsArary = [];
+const fireWidth = 7;
+const fireHeight = 5;
+
+function start(){
+	createFireDataStructure();
+	renderFire();
+}
+
+function createFireDataStructure() {
+	const numberOfPixels = fireWidth * fireHeight;
+	for(let i = 0; i < numberOfPixels; i++){
+		firePixelsArary[i]=0;
+	}
+}
+
+function createFirePropagation() {
+	
+}
+
+function renderFire() {
+	let html = '<table cellpadding=0 cellspacing=0>';
+	for (let row = 0; row < fireHeight; row++) {
+		html += '<tr>';
+		for (let column = 0; column < fireWidth; column++) {
+			const pixelIndex = column + (fireWidth * row);
+			html += '<td>';
+			html += pixelIndex;
+			html += '</td>';
+		}
+		html += '</tr>';
+	}
+	html += '</table>';
+	document.querySelector('#fireCanvas').innerHTML = html;
+}
+
+
+start();
