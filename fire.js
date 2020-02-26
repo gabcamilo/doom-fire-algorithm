b036b9c4e7d2d8ce4232ac6f7e8c2e5ecd8d42b9
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-02-26 08:50:28 -0300

    createFireSource function

--- fire.js
+++ fire.js
@@ -1,9 +1,10 @@
 const firePixelsArary = [];
-const fireWidth = 7;
-const fireHeight = 5;
+const fireWidth = 10;
+const fireHeight = 10;
 
 function start(){
 	createFireDataStructure();
+	createFireSource();
 	renderFire();
 }
 
@@ -24,14 +25,24 @@ function renderFire() {
 		html += '<tr>';
 		for (let column = 0; column < fireWidth; column++) {
 			const pixelIndex = column + (fireWidth * row);
+			const fireIntensity = firePixelsArary[pixelIndex];
 			html += '<td>';
 			html += `<div class="pixel-index">${pixelIndex}</div>`;
+			html += fireIntensity;
 			html += '</td>';
 		}
 		html += '</tr>';
 	}
 	html += '</table>';
 	document.querySelector('#fireCanvas').innerHTML = html;
+}
+
+function createFireSource() {
+	for (let column = 0; column <= fireWidth; column++) {
+		const overflowPixelIndex = fireWidth * fireHeight;
+		const pixelIndex = (overflowPixelIndex - fireWidth) + column;
+		firePixelsArary[pixelIndex] = 36
+	}
 }
 
 start();
